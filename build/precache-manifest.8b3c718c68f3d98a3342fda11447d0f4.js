7f4f91ee784633e9aa06ebc7f688fced32326458
nikitparajuli <nikitparajuli@yahoo.com>
2020-04-12 21:02:35 -0400

    Initial build

---
+++ build/precache-manifest.8b3c718c68f3d98a3342fda11447d0f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785dc21cad44df66b39c22c5e25746f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1051eebacd8047d030e9",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "840b0bc1db0f668ec41a",
+    "url": "/static/css/main.b67bf3d1.chunk.css"
+  },
+  {
+    "revision": "1051eebacd8047d030e9",
+    "url": "/static/js/2.c160f0de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c160f0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840b0bc1db0f668ec41a",
+    "url": "/static/js/main.116fed90.chunk.js"
+  },
+  {
+    "revision": "14ae77c298297d00e365",
+    "url": "/static/js/runtime-main.66826c9a.js"
+  }
+]);
